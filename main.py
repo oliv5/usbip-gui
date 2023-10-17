995f3d3afc0d1e2ea698c8932d32ecf618db63e8
Olivier Lanneluc <olivier.lanneluc@ubiik.com>
2023-10-17 17:03:56 +0200

    Quick fixes for my case

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 import subprocess
 
 
-subprocess.call(["gksudo", "python3.8", "gui.py"])
+subprocess.call(["sudo", "python3", "gui.py"])
 #TODO now make a simple window to manage servers, devices on servers etc
 
 #TODO also make a simple install script to fetch the usbip and linxu-tools-generic packages and modprobe the modules
